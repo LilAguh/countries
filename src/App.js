1e17d9630a899325a054e8d7dc2ac812e7097c9e
LilAguh <aguezeochoa@gmail.com>
2022-10-18 22:44:44 -0300

    the get countries endpoint is executed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import { useSelector } from 'react-redux'
+import { useGetAllCountriesQuery } from "./features/comments/commentsApi"
 function App() {
+
+  const { data, isLoading, error } = useGetAllCountriesQuery();
+
+  console.log(data)
+
   return (
-    <h1>hello world</h1>
+    <div>
+      <h1>hello world</h1>
+    </div>
   );
 }
 
 export default App;
+
+
+
