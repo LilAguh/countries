9128f5bbf288a1a9cd71401fec700ea179b1c010
LilAguh <aguezeochoa@gmail.com>
2022-10-20 23:07:58 -0300

    create components folder width Switch of darkmode inside

---
+++ src/components/switchMode/SwitchMode.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./SwitchMode.css"
+// import { useDispatch, useSelector } from "react-redux"
+
+export default function SwitchMode() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    return (
+        <div>
+            <input
+                type="checkbox"
+                id="switch"
+                checked={isChecked}
+                onChange={handleOnChange}
+            />
+            <label for="switch"></label>
+        </div>
+    );
+}
